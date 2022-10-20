9363d97a0198372bc459f53fbea997cc002dc981
R0binj0 <113966762+R0binj0@users.noreply.github.com>
2022-10-20 15:16:27 +0300

    Update yl6.py

--- yl6.py
+++ yl6.py
@@ -1,6 +1,6 @@
-a = int(input("Lisa esimene number:"))
-b = int(input("Lisa teine number:"))
-c = int(input("Lisa kolmas number:"))
+a = int(input("Esimene number:"))
+b = int(input("Teine number:"))
+c = int(input("Kolmas number:"))
 
 def suurim(a, b, c):
     if (a >= b) and (a >= c):
